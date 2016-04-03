73fd11204d753d5992fa18f4536390181b7fd7c8
testDrive64 <com9on@me.com>
2016-04-03 02:52:05 +0200

    put some data in

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var app = angular.module('impNews', []);
 
 app.controller('MainCtrl', [
   '$scope',
+  $scope.posts = [
+    'post 1',
+    'post 2',
+    'post 3',
+    'post 4',
+    'post 5'
+  ];
+  
   function($scope) {
     $scope.test = 'Hello world!';
   }
